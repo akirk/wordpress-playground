59a546815cee429708d543516ec6addc9b55ec9a
Alex Kirk <akirk@users.noreply.github.com>
2026-01-03 01:06:40 +0100

    Don't upgrade HTTP to HTTPS on localhost

--- packages/php-wasm/web/src/lib/fetch-with-cors-proxy.ts
+++ packages/php-wasm/web/src/lib/fetch-with-cors-proxy.ts
@@ -15,7 +15,12 @@ export async function fetchWithCorsProxy(
 	let requestUrlObj = playgroundUrlObj
 		? new URL(requestObject.url, playgroundUrlObj)
 		: new URL(requestObject.url);
-	if (requestUrlObj.protocol === 'http:') {
+	const isLocalhost =
+		requestUrlObj.hostname === 'localhost' ||
+		requestUrlObj.hostname === '127.0.0.1' ||
+		requestUrlObj.hostname === '[::1]' ||
+		requestUrlObj.hostname === '::1';
+	if (requestUrlObj.protocol === 'http:' && !isLocalhost) {
 		requestUrlObj.protocol = 'https:';
 		const httpsUrl = requestUrlObj.toString();
 		requestObject = await cloneRequest(requestObject, { url: httpsUrl });
